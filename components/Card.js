42b4b1f6d55e03ae18c27a4c72fafee7db208dcb
othmane <othman-19@hotmail.fr>
2021-02-13 12:46:08 +0100

    Implement a Card component

---
+++ components/Card.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+
+const Card = ({ children, style }) => (
+  <View style={{ ...styles.card, ...style }}>
+    {children}
+  </View>
+);
+
+const styles = StyleSheet.create({
+  card: {
+    borderRadius: 10,
+    alignItems: 'center',
+    elevation: 6,
+    shadowColor: 'black',
+    shadowRadius: 6,
+    shadowOpacity: 0.6,
+    shadowOffset: {
+      height: 2,
+      width: 0,
+    },
+    backgroundColor: 'white',
+  },
+  headerTitle: {
+    color: 'black',
+    fontSize: 18,
+  },
+});
+
+export default Card;
